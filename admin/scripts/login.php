fa340fcb4f4ddf0fe24bba6c4d78a853cd00bf41
Mariam Khalifa <mariamk@Mariams-MacBook-Pro.local>
2020-04-01 09:53:13 -0400

    password hash

--- admin/scripts/login.php
+++ admin/scripts/login.php
@@ -12,44 +12,63 @@ function login($username, $password, $ip){
 
     //echo $check_exist_query;exit;
 
-    if($user_set->fetchColumn()>0){
-        //Check if match
-        $check_match_query = 'SELECT * FROM `tbl_users` WHERE user_username =:username';
-        $check_match_query .=' AND user_password=:password';
-        $user_match = $pdo->prepare($check_match_query);
-        $user_match->execute(
-            array(
-                ':username'=>$username,
-                ':password'=>$password
-            )
-        );
+    $hashed_pass_query = 'SELECT * FROM `tbl_users` WHERE user_username =:username';
+    $user = $pdo->prepare($hashed_pass_query);
+    $user->execute(
+        array(
+            ':username'=>$username
+        )
+    );
+    $user = $user->fetch(PDO::FETCH_ASSOC);
+    $hashed_password = $user['user_password'];
 
-        //echo $check_match_query;exit;
+    if(password_verify($password,$hashed_password)){
+        $verified_password = $hashed_password;
 
-        while($founduser = $user_match->fetch(PDO::FETCH_ASSOC)){
-            $id = $founduser['user_id'];
-
-            $_SESSION['user_id'] = $id;
-            $_SESSION['user_username'] = $founduser['user_full_name'];
-
-            $update = 'UPDATE tbl_users SET user_ip=:ip WHERE user_id = :id';
-            $user_update = $pdo->prepare($update);
-            $user_update->execute(
+        if($user_set->fetchColumn()>0){
+            //Check if match
+            $check_match_query = 'SELECT * FROM `tbl_users` WHERE user_username =:username';
+            $check_match_query .=' AND user_password=:password';
+            $user_match = $pdo->prepare($check_match_query);
+            $user_match->execute(
                 array(
-                    ':ip'=>$ip,
-                    ':id'=>$id
+                    ':username'=>$username,
+                    ':password'=>$verified_password
                 )
             );
-        }
-        
-        if(isset($id)){
-            redirect_to('index.php');
-        }else{
-            return 'Wrong password';
-        }
-    }else{
 
-        return 'User does not exist!';
+            //echo $check_match_query;exit;
+
+        
+
+            while($founduser = $user_match->fetch(PDO::FETCH_ASSOC)){
+                $id = $founduser['user_id'];
+
+                $_SESSION['user_id'] = $id;
+                $_SESSION['user_username'] = $founduser['user_full_name'];
+
+                $update = 'UPDATE tbl_users SET user_ip=:ip WHERE user_id = :id';
+                $user_update = $pdo->prepare($update);
+                $user_update->execute(
+                    array(
+                        ':ip'=>$ip,
+                        ':id'=>$id
+                    )
+                );
+            }
+        
+            if(isset($id)){
+                redirect_to('index.php');
+            }else{
+                return 'Wrong password';
+            }
+        }else{
+
+            return 'User does not exist!';
+        }
+
+    }else{
+        return 'passwords do not match!';
     }
 }
 
